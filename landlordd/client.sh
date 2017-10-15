d633e152e2530e2be7e936eae958866be4c80d7d
Christopher Hunt <huntchr@gmail.com>
2017-10-15 16:59:17 +1100

    Removed a useless trap command that wasn't working on Linux anyhow.

--- landlordd/client.sh
+++ landlordd/client.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-trap 'cat <<< "hi"' SIGTERM SIGINT
 ( \
   echo "-cp classes example.Hello" && \
-  tar -c -C /Users/huntc/Projects/farmco/landlord/landlordd/test/target/scala-2.12 classes &&
+  tar -c -C test/target/scala-2.12 classes &&
   while read x ; do echo $x ; done && \
   printf '\04' \
   ) | nc 127.0.0.1 9000 &
