cf5580bde506a644e1d07fa2860089309f3aea58
Christopher Hunt <huntchr@gmail.com>
2017-10-15 17:40:52 +1100

    Corrected path

--- landlordd/client.sh
+++ landlordd/client.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ( \
   echo "-cp classes example.Hello" && \
-  tar -c -C test/target/scala-2.12 classes &&
+  tar -c -C $(pwd)/test/target/scala-2.12 classes &&
   while read x ; do echo $x ; done && \
   printf '\04' \
   ) | nc 127.0.0.1 9000 &
