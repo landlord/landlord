168b054d37f7b3deff48b9e0f88206197992e19d
Christopher Hunt <huntchr@gmail.com>
2017-10-16 10:02:47 +1100

    client script now works on Linux

--- landlordd/client.sh
+++ landlordd/client.sh
@@ -2,7 +2,6 @@
 ( \
   echo "-cp classes example.Hello" && \
   tar -c -C $(pwd)/test/target/scala-2.12 classes &&
-  while read x ; do echo $x ; done && \
+  cat <&0 && \
   printf '\04' \
-  ) | nc 127.0.0.1 9000 &
-wait
+  ) | nc 127.0.0.1 9000
