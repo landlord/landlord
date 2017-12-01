b51d6cf6b162459ca73490c386c38b9eb3cffd0e
Christopher Hunt <huntchr@gmail.com>
2017-12-01 16:13:05 +1100

    Gracefully shutdown processes on shutting down Landlord

--- landlordd/daemon/src/test/scala/com/github/huntc/landlord/MainSpec.scala
+++ landlordd/daemon/src/test/scala/com/github/huntc/landlord/MainSpec.scala
@@ -2,14 +2,15 @@ package com.github.huntc.landlord
 
 import java.nio.ByteOrder
 
-import akka.NotUsed
-import akka.actor.{ Actor, ActorSelection, ActorSystem, Props }
+import akka.{ Done, NotUsed }
+import akka.actor.{ Actor, ActorSelection, ActorSystem, PoisonPill, Props }
 import akka.stream.scaladsl.{ Sink, Source }
 import akka.stream.ActorMaterializer
 import akka.testkit._
 import akka.util.ByteString
 import org.scalatest._
 
+import scala.concurrent.duration._
 import scala.concurrent.Promise
 import scala.util.Try
 
@@ -30,6 +31,8 @@ class MainSpec extends TestKit(ActorSystem("MainSpec"))
 
   "Main" should {
     "process input for launching a process" in {
+      val reaper = TestProbe()
+
       def launchInfoOp(in: Source[ByteString, NotUsed], out: Promise[Source[ByteString, NotUsed]]): (Int, Props) =
         123 -> Props(new Actor {
           override def preStart(): Unit = {
@@ -42,32 +45,70 @@ class MainSpec extends TestKit(ActorSystem("MainSpec"))
           }
         })
 
-      Source
-        .single(ByteString("lsomeinput"))
-        .via(Main.controlFlow(launchInfoOp, emptySendKillOp))
-        .runWith(Sink.head)
-        .map(r => assert(r.utf8String == "someinput"))
+      val result =
+        Source
+          .single(ByteString("lsomeinput"))
+          .via(Main.controlFlow(reaper.ref, launchInfoOp, emptySendKillOp))
+          .runWith(Sink.head)
+
+      reaper.expectMsgClass(classOf[Main.JvmExecutorReaper.Register])
+      result.map(r => assert(r.utf8String == "someinput"))
     }
 
     "receive a process id and signal when killing a process" in {
+      val reaper = TestProbe()
+
       val result = Promise[Assertion]()
       def sendKillOp(jvmExecutor: ActorSelection, signal: Int): Unit =
-        result.complete(Try(assert(jvmExecutor.pathString == "/user/123" && signal == 15)))
+        result.complete(Try(assert(jvmExecutor.pathString == "/user/process-123" && signal == 15)))
 
       Source
         .single(ByteString.newBuilder.putByte('k').putInts(Array(123, 15))(ByteOrder.BIG_ENDIAN).result())
-        .via(Main.controlFlow(emptyLaunchInfoOp, sendKillOp))
+        .via(Main.controlFlow(reaper.ref, emptyLaunchInfoOp, sendKillOp))
         .runWith(Sink.ignore)
 
+      reaper.expectNoMessage(1.second.dilated)
       result.future
     }
 
     "receive ??? with an unknown command" in {
-      Source
-        .single(ByteString.newBuilder.putByte('z').result())
-        .via(Main.controlFlow(emptyLaunchInfoOp, emptySendKillOp))
-        .runWith(Sink.head)
-        .map(r => assert(r.utf8String == "???"))
+      val reaper = TestProbe()
+
+      val result =
+        Source
+          .single(ByteString.newBuilder.putByte('z').result())
+          .via(Main.controlFlow(reaper.ref, emptyLaunchInfoOp, emptySendKillOp))
+          .runWith(Sink.head)
+
+      reaper.expectNoMessage(1.second.dilated)
+      result.map(r => assert(r.utf8String == "???"))
+    }
+  }
+
+  "JvmExecutorReaper" should {
+    import Main.JvmExecutorReaper
+    "register two actors and receive done with one having stopped and the other acting upon shutting down" in {
+      val executor0 = TestProbe()
+      val executor1 = TestProbe()
+      val reaper = system.actorOf(JvmExecutorReaper.props)
+      reaper ! JvmExecutorReaper.Register(executor0.ref)
+      reaper ! JvmExecutorReaper.Register(executor1.ref)
+      executor0.ref ! PoisonPill
+      val shutdownRequestor = TestProbe()
+      shutdownRequestor.send(reaper, JvmExecutorReaper.Shutdown)
+      executor0.expectNoMessage(1.second.dilated)
+      executor1.expectMsg(JvmExecutor.SignalProcess(JvmExecutor.SIGTERM))
+      executor1.ref ! PoisonPill
+      shutdownRequestor.expectMsg(Done)
+      succeed
+    }
+
+    "don't register an actor but receive done upon shutting down" in {
+      val reaper = system.actorOf(JvmExecutorReaper.props)
+      val shutdownRequestor = TestProbe()
+      shutdownRequestor.send(reaper, JvmExecutorReaper.Shutdown)
+      shutdownRequestor.expectMsg(Done)
+      succeed
     }
   }
 }
