c60528ad82ab044f0ae7d21b28d54c6f7dec1de6
Christopher Hunt <huntchr@gmail.com>
2018-01-06 19:10:48 +1100

    Dependency refresh
    Excluding commons-compress given that 1.1.5 has some breaking change - suspecting something to do with https://issues.apache.org/jira/browse/COMPRESS-407

--- landlordd/project/Dependencies.scala
+++ landlordd/project/Dependencies.scala
@@ -2,16 +2,16 @@ import sbt._
 import sbt.Resolver.bintrayRepo
 
 object Versions {
-  lazy val akka = "2.5.6"
+  lazy val akka = "2.5.8"
   lazy val commonsCompress = "1.14"
   lazy val logbackClassic = "1.2.3"
   lazy val jnr = "0.18"
-  lazy val scalaTest = "3.0.3"
+  lazy val scalaTest = "3.0.4"
   lazy val scopt = "3.7.0"
 }
 
 object Dependencies {
-  lazy val akkaStream = ("com.typesafe.akka" %% "akka-stream" % Versions.akka)
+  lazy val akkaStream = "com.typesafe.akka" %% "akka-stream" % Versions.akka
   lazy val akkSlf4j = "com.typesafe.akka" %% "akka-slf4j" % Versions.akka
   lazy val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % Versions.akka
   lazy val commonsCompress = "org.apache.commons" % "commons-compress" % Versions.commonsCompress
--- landlordd/project/plugins.sbt
+++ landlordd/project/plugins.sbt
@@ -1,3 +1,3 @@
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.5")
-addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.3.1")
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.6")
+addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.3.2")
 addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.8.2")
