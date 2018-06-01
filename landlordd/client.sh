89f544afd695e32712dd4e20c1cf83277fccb535
Christopher Hunt <huntchr@gmail.com>
2018-06-01 18:40:09 +1000

    Support docker and Unix Domain socket mounts

--- landlordd/client.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-( \
-  printf "l" && \
-  printf -- $"l-cp\0classes\0example.Hello\n" && \
-  tar -c -C $(pwd)/test/target/scala-2.12 classes && \
-  cat <&0 \
-  ) | \
-nc -U /var/run/landlord/landlordd.sock
